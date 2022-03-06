1d203b829f02db2d50fc69c1d8969a4bd3e6ba8e
EC2 Default User <ec2-user@ip-172-31-20-227.ec2.internal>
2022-03-06 07:47:06 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,57 @@
+class BooksController < ApplicationController
+  def new
+    @books = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to books_path
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = current_user
+    @book_new = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = "You have updated book successfully."
+       redirect_to book_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    @user = current_user
+    @book = Book.new
+  end
+  
+  def show
+    @user = current_user
+    @user_f = User.find(params[:id])
+    @book = Book.new
+    @books = @user_f.books
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+       flash[:notice] = "You have updated user successfully."
+    redirect_to user_path
+    else
+      render :edit
+    end  
+  end 
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+end
